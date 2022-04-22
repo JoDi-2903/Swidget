ad6032959d6f4328ff3ca90a91d46ddfdefb6bd9
Jonathan Diebel <jonathan.diebel@web.de>
2022-04-23 01:42:05 +0200

    Initial Commit

---
+++ Swidget/SwidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwidgetApp.swift
+//  Swidget
+//
+//  Created by Jonathan Diebel on 23.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
