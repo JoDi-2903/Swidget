112fee5bfe5fad473a74c243370cb8dda54d363e
Jonathan Diebel <jonathan.diebel@web.de>
2022-06-04 13:50:27 +0200

    added posterURL and backdropURL to movie

--- Swidget/Models/Movie.swift
+++ Swidget/Models/Movie.swift
@@ -14,11 +14,19 @@ struct MovieResponse: Decodable {
 struct Movie: Decodable {
     let id: Int
     let title: String
-    let backdropPath: String?
     let posterPath: String?
+    let backdropPath: String?
     let overview: String
     let voteAverage: Double
     let voteCount: Int
     let runtime: Int?
     let releaseDate: String?
+    
+    var posterURL: URL {
+        return URL(string: "https://image.tmdb.org/t/p/w500\(posterPath ?? "")")!
+    }
+    
+    var backdropURL: URL {
+        return URL(string: "https://image.tmdb.org/t/p/w500\(backdropPath ?? "")")!
+    }
 }
