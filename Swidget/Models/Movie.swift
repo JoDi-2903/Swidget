ebaf9009d6565f37ca21e8dcf50be1f56a56e775
Jonathan Diebel <jonathan.diebel@web.de>
2022-06-04 16:37:27 +0200

    added rating, runtime and year to movie model

--- Swidget/Models/Movie.swift
+++ Swidget/Models/Movie.swift
@@ -21,6 +21,7 @@ struct Movie: Decodable, Identifiable {
     let voteCount: Int
     let runtime: Int?
     let releaseDate: String?
+    let genres: [MovieGenre]?
     
     var posterURL: URL {
         return URL(string: "https://image.tmdb.org/t/p/w500\(posterPath ?? "")")!
@@ -29,4 +30,65 @@ struct Movie: Decodable, Identifiable {
     var backdropURL: URL {
         return URL(string: "https://image.tmdb.org/t/p/w500\(backdropPath ?? "")")!
     }
+    
+    var genreText: String {
+        genres?.first?.name ?? "n/a"
+    }
+    
+    // Convert Rating to Stars
+    var ratingStars: String {
+        let rating = Int(voteAverage)
+        if (rating > 0) {
+            let ratingInStars = (Double(rating) * 0.5)
+            switch ratingInStars {
+            case 1:
+                return "★☆☆☆☆"
+            case 2:
+                return "★★☆☆☆"
+            case 3:
+                return "★★★☆☆"
+            case 4:
+                return "★★★★☆"
+            case 5:
+                return "★★★★★"
+            default:
+                return "☆☆☆☆☆"
+            }
+        } else {
+            return "n/a"
+        }
+    }
+    
+    // Extract year from release date
+    static private let yearFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy"
+        return formatter
+    }()
+    
+    var yearText: String {
+        guard let releaseDate = self.releaseDate, let date = Utils.dateFormatter.date(from: releaseDate) else {
+            return "n/a"
+        }
+        return Movie.yearFormatter.string(from: date)
+    }
+    
+    // Convert runtime of movie to hours and minutes
+    static private let durationFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .full
+        formatter.allowedUnits = [.hour, .minute]
+        return formatter
+    }()
+    
+    var durationText: String {
+        guard let runtime = self.runtime, runtime > 0 else {
+            return "n/a"
+        }
+        return Movie.durationFormatter.string(from: TimeInterval(runtime) * 60) ?? "n/a"
+    }
+}
+
+struct MovieGenre: Decodable {
+    let name: String
 }
