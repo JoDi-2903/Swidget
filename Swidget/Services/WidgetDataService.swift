ac8d48561e7b6312c15f2bcc8ad9fabe30243740
Jonathan Diebel <jonathan.diebel@web.de>
2022-06-09 15:48:52 +0200

    updated widgets to work with real api data

--- Swidget/Services/WidgetDataService.swift
+++ Swidget/Services/WidgetDataService.swift
@@ -14,8 +14,20 @@ final class WidgetDataService {
     private let baseAPIURL = "https://api.themoviedb.org/3"
     private let jsonDecoder = Utils.jsonDecoder
     
+    // generic helper function to fetch data from given URL
+    private func fetch<T: Decodable>(from endpoint: String) async throws -> T {
+        let urlString = baseAPIURL + endpoint + "?api_key=" + apiKey + "&language=en-US"
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, urlResponse) = try await URLSession.shared.data(from: url)
+        let result = try jsonDecoder.decode(T.self, from: data)
+        return result
+    }
+    
     func getMovieFromId(id: Int) async throws -> Movie {
-        let movie: Movie = .placeholder(42)
+        let movie: Movie = try await fetch(from: "/movie/\(id)")
         return movie
     }
 }
