50c342160712ccec7ab9ff4963ef767b74124f39
Jonathan Diebel <jonathan.diebel@web.de>
2022-06-12 23:30:21 +0200

    updated StartPageView

--- Swidget/Views/StartPageView.swift
+++ Swidget/Views/StartPageView.swift
@@ -9,9 +9,33 @@ import SwiftUI
 
 struct StartPageView: View {
     var body: some View {
-        VStack {
-            Text("Hello, Swidget!")
-                .padding()
+        NavigationView {
+            Form {
+                Section {
+                    VStack(alignment: .leading) {
+                        Image("WidgetKit_TMDB_Icons")
+                                    .resizable()
+                                    .scaledToFit()
+                        
+                        Spacer(minLength: 5)
+                        
+                        Text("Hello, Swidget! Some descriptive text ...")
+                    }
+                    .padding(.all, 10)
+                }
+                Section {
+                    Text("Add widgets to your Home Screen")
+                        .font(.title3)
+                        .bold()
+                        .padding(.all, 5)
+                        //.padding(.bottom, 10)
+                    Text("1. From the Home Screen, touch and hold a widget or an empty area until the apps jiggle.")
+                    Text("2. Tap the Add button Grey add button in the top left-hand corner.")
+                    Text("3. Select a widget, choose from the three different widget sizes, then tap Add Widget.")
+                    Text("4. Tap Done.")
+                }
+            }
+            .navigationTitle("Welcome to Swidget")
         }
     }
 }
