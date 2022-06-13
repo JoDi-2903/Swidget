c8eb523182a51ff6dbe9cba8e6db343f9aae3c24
Jonathan Diebel <jonathan.diebel@web.de>
2022-06-13 18:24:11 +0200

    started building MovieOverview Page

--- Swidget/Views/MoviesOverviewView.swift
+++ Swidget/Views/MoviesOverviewView.swift
@@ -9,33 +9,54 @@ import SwiftUI
 
 struct MoviesOverviewView: View {
     
-    @ObservedObject private var nowPlayingState = MovieListState()
-    @ObservedObject private var upcomingState = MovieListState()
-    @ObservedObject private var topRatedState = MovieListState()
-    @ObservedObject private var popularState = MovieListState()
+    @State var nowPlayingMovies: [Movie] = []
+    @State var popularMovies: [Movie] = []
+    @State var topRatedMovies: [Movie] = []
+    @State var upcomingMovies: [Movie] = []
+    @State var selectedMovie: Movie?
+    
+//    @ObservedObject private var nowPlayingState = MovieListState()
+//    @ObservedObject private var upcomingState = MovieListState()
+//    @ObservedObject private var topRatedState = MovieListState()
+//    @ObservedObject private var popularState = MovieListState()
     
     var body: some View {
         NavigationView {
-            VStack {
-                HStack {
-                    List {
-                        Group {
-                            if nowPlayingState.movies != nil {
-                                MovieSliderView(title: "Now Playing", movies: nowPlayingState.movies!)
-                                
-                            } else {
-                                Text("Error while loading NowPlaying Slider!")
-                            }
-                        }
-                        .listRowInsets(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
-                    }
-                    .navigationBarTitle("Movies Overview")
-                }
+            
+        }
+        .navigationTitle("Movies Overview")
+        .task {
+            do {
+                nowPlayingMovies = try await WidgetDataService.shared.getMoviesFromCategory(category: "now_playing", language: "en-US")
+                popularMovies = try await WidgetDataService.shared.getMoviesFromCategory(category: "popular", language: "en-US")
+                topRatedMovies = try await WidgetDataService.shared.getMoviesFromCategory(category: "top_rated", language: "en-US")
+                upcomingMovies = try await WidgetDataService.shared.getMoviesFromCategory(category: "upcoming", language: "en-US")
+            } catch {
+                print("Error loading the movies from API! \(error)")
             }
         }
-        .onAppear {
-            self.nowPlayingState.loadMovies(with: .nowPlaying)
-        }
+        
+//        NavigationView {
+//            VStack {
+//                HStack {
+//                    List {
+//                        Group {
+//                            if nowPlayingState.movies != nil {
+//                                MovieSliderView(title: "Now Playing", movies: nowPlayingState.movies!)
+//
+//                            } else {
+//                                Text("Error while loading NowPlaying Slider!")
+//                            }
+//                        }
+//                        .listRowInsets(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
+//                    }
+//                    .navigationBarTitle("Movies Overview")
+//                }
+//            }
+//        }
+//        .onAppear {
+//            self.nowPlayingState.loadMovies(with: .nowPlaying)
+//        }
     }
 }
 
